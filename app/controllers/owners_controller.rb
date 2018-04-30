71392f2f5faf17fcadf3b9b2b94e8a1ee0aad26c
github username <github email address>
2018-04-30 17:25:11 -0400

    deleted @owner.save and added comments in owners_controller.rb

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -2,35 +2,37 @@ class OwnersController < ApplicationController
 
   get '/owners' do
     @owners = Owner.all
-    erb :'/owners/index' 
+    erb :'/owners/index'
   end
 
-  get '/owners/new' do 
+  get '/owners/new' do
     @pets = Pet.all
     erb :'/owners/new'
   end
 
-  post '/owners' do 
+  post '/owners' do
     @owner = Owner.create(params[:owner])
     if !params["pet"]["name"].empty?
       @owner.pets << Pet.create(name: params["pet"]["name"])
+      # When using the shovel operator, ActiveRecord instantly fires update SQL
+      # without waiting for the save or update call on the parent object,
+      # unless the parent object is a new record.
     end
-    @owner.save
     redirect "owners/#{@owner.id}"
   end
 
-  get '/owners/:id/edit' do 
+  get '/owners/:id/edit' do
     @owner = Owner.find(params[:id])
     @pets = Pet.all
     erb :'/owners/edit'
   end
 
-  get '/owners/:id' do 
+  get '/owners/:id' do
     @owner = Owner.find(params[:id])
     erb :'/owners/show'
   end
 
-  post '/owners/:id' do 
+  post '/owners/:id' do
     @owner = Owner.find(params[:id])
     @owner.update(params["owner"])
     if !params["pet"]["name"].empty?
