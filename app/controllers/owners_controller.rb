19b45c956b805de7bca29e7a515416381da4818d
Daniel Ran <danielr1735@gmail.com>
2018-07-19 12:14:02 -0400

    bug fix - edited post '/owners/:id' method in owners_controller.rb to enable removing ALL pets from owner

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -34,6 +34,13 @@ class OwnersController < ApplicationController
 
   post '/owners/:id' do
     @owner = Owner.find(params[:id])
+    
+    ####### the following bug fix is required so that it's possible to remove ALL previous pets from owner.
+    if !params[:owner].keys.include?("pet_ids")
+    params[:owner]["pet_ids"] = []
+    end
+    ####### End of fix
+
     @owner.update(params["owner"])
     if !params["pet"]["name"].empty?
       @owner.pets << Pet.create(name: params["pet"]["name"])
