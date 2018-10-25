7ca673299a82d6119e80605b6038be4f024bc076
mmacdonald1 <melmacd94@gmail.com>
2018-10-25 11:10:48 -0400

    post to patch

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,14 +2,14 @@ class PetsController < ApplicationController
 
   get '/pets' do
     @pets = Pet.all
-    erb :'/pets/index' 
+    erb :'/pets/index'
   end
 
-  get '/pets/new' do 
+  get '/pets/new' do
     erb :'/pets/new'
   end
 
-  post '/pets' do 
+  post '/pets' do
     @pet = Pet.create(params[:pet])
     if !params["owner"]["name"].empty?
       @pet.owner = Owner.create(name: params["owner"]["name"])
@@ -18,12 +18,12 @@ class PetsController < ApplicationController
     redirect "pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do 
+  get '/pets/:id' do
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
 
-  post '/pets/:id' do 
+  patch '/pets/:id' do 
     @pet = Pet.find(params[:id])
     @pet.update(params["pet"])
     if !params["owner"]["name"].empty?
@@ -33,7 +33,7 @@ class PetsController < ApplicationController
     redirect "pets/#{@pet.id}"
   end
 
-  get '/pets/:id/edit' do 
+  get '/pets/:id/edit' do
     @pet = Pet.find(params[:id])
     erb :'/pets/edit'
   end
