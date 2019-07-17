5d0687060ee0bd88ecf23828c5668c3276b410ae
Kellye Greene <kellyecodes@gmail.com>
2019-07-16 23:02:50 -0400

    Revert "Wip solution update"

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -2,44 +2,28 @@ class OwnersController < ApplicationController
 
   get '/owners' do
     @owners = Owner.all
-    erb :'/owners/index'
+    erb :'/owners/index' 
   end
 
-  get '/owners/new' do
-    @pets = Pet.all
+  get '/owners/new' do 
     erb :'/owners/new'
   end
 
-  post '/owners' do
-    @owner = Owner.create(params[:owner])
-    if !params["pet"]["name"].empty?
-      @owner.pets << Pet.create(name: params["pet"]["name"])
-      # When using the shovel operator, ActiveRecord instantly fires update SQL
-      # without waiting for the save or update call on the parent object,
-      # unless the parent object is a new record.
-    end
-    redirect "/owners/#{@owner.id}"
+  post '/owners' do 
+    
   end
 
-  get '/owners/:id/edit' do
+  get '/owners/:id/edit' do 
     @owner = Owner.find(params[:id])
-    @pets = Pet.all
     erb :'/owners/edit'
   end
 
-  get '/owners/:id' do
+  get '/owners/:id' do 
     @owner = Owner.find(params[:id])
     erb :'/owners/show'
   end
 
-  patch '/owners/:id' do
-    @owner = Owner.find(params[:id])
-    @owner.update(params[:owner])
-
-    if !params["pet"]["name"].empty?
-      @owner.pets << Pet.create(name: params["pet"]["name"])
-    end
-
-    redirect "/owners/#{@owner.id}"
+  patch '/owners/:id' do 
+   
   end
 end
--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,46 +2,24 @@ class PetsController < ApplicationController
 
   get '/pets' do
     @pets = Pet.all
-    erb :'/pets/index'
+    erb :'/pets/index' 
   end
 
-  get '/pets/new' do
+  get '/pets/new' do 
     erb :'/pets/new'
   end
 
-  post '/pets' do
-    @pet = Pet.create(params[:pet])
-
-    if !params["owner"]["name"].empty?
-      @pet.owner = Owner.create(name: params["owner"]["name"])
-    end
-
-    @pet.save
+  post '/pets' do 
 
     redirect to "pets/#{@pet.id}"
   end
 
-  get '/pets/:id' do
+  get '/pets/:id' do 
     @pet = Pet.find(params[:id])
     erb :'/pets/show'
   end
 
-  get '/pets/:id/edit' do
-    @pet = Pet.find(params[:id])
-    erb :'/pets/edit'
-  end
-
-  patch '/pets/:id' do
-    @pet = Pet.find(params[:id])
-    @pet.update(params[:pet])
-
-    #binding.pry
-
-    if !params["owner"]["name"].empty?
-      @pet.owner = Owner.create(name: params["owner"]["name"])
-    end
-
-    @pet.save
+  patch '/pets/:id' do 
 
     redirect to "pets/#{@pet.id}"
   end
